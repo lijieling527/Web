52970441a0121898364b67b37d5dead1424d596f
lijieling527 <qoo40328@gmail.com>
2023-06-01 01:41:29 +0800

    eleventh

---
+++ portfolio-1/lab7/main.js
@@ -0,0 +1,27 @@
+var ctx, thisImage;
+$(function(){
+    $("[type='date']").on("change",showDate);
+    ctx=$("#myCanvas")[0].getContext("2d");
+});
+
+$(function(){
+    $("[type='color']").on("change",changeBodyBg);
+    
+});
+
+function showDate(){
+    var thisDate=this.value;
+    thisDate=thisDate.replace(/-/g,"");
+    thisImage=new Image();
+    thisImage.src="flipClockNumbers.png";
+    thisImage.onload=function() {
+        for(var x=0;x<thisDate.length;x++){
+            ctx.drawImage(thisImage, thisDate[x] *80, 0, 90, 130, 60*x, 0, 60, 100);
+        }
+    };
+}
+
+function changeBodyBg(){
+    
+    document.body.style.background = this.value;
+}
